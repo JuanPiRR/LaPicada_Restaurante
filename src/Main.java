1bfde61da59ad49012a4c992db3931d4529d3ad2
Esperanza <esperanzamonsalve2401@alumnos.ubiobio.cl>
2025-12-17 22:25:13 -0300

    detalles

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import vista.VistaPrincipal;
-
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new VistaPrincipal();
+        SwingUtilities.invokeLater(() -> new VistaPrincipal());
     }
 }
--- src/vista/VistaPrincipal.java
+++ src/vista/VistaPrincipal.java
@@ -107,9 +107,4 @@ public class VistaPrincipal extends JFrame {
         });
         setVisible(true);
     }
-
-    public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(() -> new VistaPrincipal());
-    }
 }
